d552e9a36ecaff400da276259e4424b0c4522ecb
abdulahhamad <alhmada176@gmail.com>
2022-04-08 11:35:34 +0300

    nav

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e62f774e1656a25"; }
+/******/ 		__webpack_require__.h = function() { return "10b191d6e082a976"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
